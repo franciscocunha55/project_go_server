d63719f94bbca13ebde698b1a2f3d2f51c72d3c3
Francisco Cunha <afrancisco97@gmail.com>
2022-07-25 19:30:30 +0100

    add backend and frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1
+          className="App-link"
+        
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </h1>
+  
+    </div>
+  );
+}
+
+export default App;
--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
-	fmt.Println("hello Alexandra")
-	// Connection DB 
-}
